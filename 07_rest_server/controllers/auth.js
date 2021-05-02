6c57075b4b5b5e8c8db1addac503876de785d2de
Oscar Chavez <oscarchavez@MacBook-Pro-de-Oscar.local>
2021-05-02 14:07:52 -0700

    login con google

--- 07_rest_server/controllers/auth.js
+++ 07_rest_server/controllers/auth.js
@@ -3,7 +3,9 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verify')
 
+///login normal con data personal 
 const login = async(req, res = response) => {
 
     const { correo, password } = req.body;
@@ -45,9 +47,55 @@ const login = async(req, res = response) => {
         });
     }
 
+}
 
+
+//login con google 
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            //tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            }
+            usuario = new Usuario(data)
+            await usuario.save();
+        }
+
+        // si el usuario en mongoDB
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el admin, usuario Bloquado'
+            })
+        }
+
+        // generar el JWT 
+        const token = await generarJWT(usuario.id);
+
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de google no es valido'
+        })
+    }
 }
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
